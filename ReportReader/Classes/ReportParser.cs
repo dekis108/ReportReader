cd8af6c36c903500d92153159754e7fe26bb2456
dejan.subasic <dejan.subasic@gmail.com>
2021-09-27 12:55:16 +0200

    ParseSummaryReport

--- ReportReader/Classes/ReportParser.cs
+++ ReportReader/Classes/ReportParser.cs
@@ -7,9 +7,65 @@ namespace ReportReader.Classes
 {
     public static class ReportParser
     {
-        public static void ParseSummaryReport()
+        public static List<Report> ParseSummaryReport(string path, string fileName)
         {
-            throw new NotImplementedException();
+            path = path + fileName;
+
+            string[] temp = path.Split('\\');
+            string directory = temp[temp.Length - 2];
+            string errorContent = "";
+            List<Report> reports = new List<Report>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] words = line.Split(' ');
+
+
+                        while (words[0] == "Error" && words[1] == "description")
+                        {
+                            do
+                            {
+                                errorContent += line + '\n';
+                                if ((line = reader.ReadLine()) == null) {
+                                    break;
+                                }
+                                
+                                words = line.Split(' ');
+                            } while (words[0] != "Error" || words[1] != "description") ;
+
+
+                            reports.Add(
+                                new Report()
+                                {
+                                    CircuitName = "TODO", //TODO
+                                    File_Content = errorContent.Trim(),
+                                    File = fileName,
+                                    Date = path.Split("at_")[1],
+                                    Log_Directory = directory,
+                                    File_State = "TODO" //TODO
+                                }
+                            );
+
+                            errorContent = line;
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+
+            return reports;
         }
 
         public static List<Report> ParseTransformReport(string path, string fileName)
--- ReportReader/Program.cs
+++ ReportReader/Program.cs
@@ -10,15 +10,20 @@ namespace ReportReader
         const string warningsFile = "Warnings.csv";
         const string errorsFile = "Errors.csv";
         const string statisticsFile = "Statistics.csv";
-        const string summary = "Summary.csv";
-        const string transformFile = "CIMToDMSTranformReports.txt";
+        const string summaryFile = "Summary.csv";
+        const string transformReport = "CIMToDMSTranformReports.txt";
+        const string summaryReport = "SummaryReport.txt";
 
         static void Main(string[] args)
         {
-            string inputPath = @"C:\Users\Dejan\Desktop\DMSZadatak\Zadatak\Reports\Reports\Rejected\Reports_for_ChangeSet_FRS_10_23_E0101 [Bulk] _created at_2014-03-04_17-40-08\";
+            string inputTransformPath = @"C:\Users\Dejan\Desktop\DMSZadatak\Zadatak\Reports\Reports\Rejected\Reports_for_ChangeSet_FRS_10_23_E0101 [Bulk] _created at_2014-03-04_17-40-08\";
+            string inputSummaryPath = @"C:\Users\Dejan\Desktop\DMSZadatak\Zadatak\Reports\Reports\Rejected\Reports_for_ChangeSet_BRT_10_57_E0101 [Bulk] _created at_2014-03-04_17-29-48\";
 
-            var records = ReportParser.ParseTransformReport(inputPath, transformFile);
-            CsvWriter.AppendOrCreate(outputPath, warningsFile, records);
+            //var records = ReportParser.ParseTransformReport(inputTransformPath, transformReport);
+            //CsvWriter.AppendOrCreate(outputPath, warningsFile, records);
+
+            var records = ReportParser.ParseSummaryReport(inputSummaryPath, summaryReport);
+            CsvWriter.AppendOrCreate(outputPath, summaryFile, records);
 
             Console.WriteLine("Done!");
             Console.ReadLine();
